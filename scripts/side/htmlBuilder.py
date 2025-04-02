b6b8dc01273e3c2b3d855bb7bdf39736557132ac
Bittu5134 <ssr7a5134@gmail.com>
2025-04-02 09:10:10 +0530

    Update htmlBuilder.py

--- scripts/side/htmlBuilder.py
+++ scripts/side/htmlBuilder.py
@@ -13,6 +13,13 @@ for file_index,file in enumerate(os.listdir("chapters/cont")):
     file_index = int(file.replace(".txt",""))-1
     with open(f"./chapters/cont/{file}", "r", encoding="utf-8") as f:
         textStr = f.read()
+        def replace_match(match):
+            original_tag = match.group(0)
+            inner_text = original_tag[1:-1]  # Extract text between < and >
+            escaped_text = html.escape(inner_text)
+            return f"<{escaped_text}>"
+        pattern = r'<.{3,}>'
+        textStr = re.sub(pattern, replace_match, textStr)
         text = textStr.split("\n")
 
     with open("website/stories/cont/read/template.html","r",encoding="utf-8") as f:
